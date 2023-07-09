15d88982e0210f7c06a985395e4b27ddc2954195
Jimmy Wärting <jimmy@warting.se>
2023-07-09 15:19:05 +0200

    Update index.js
    remove export

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 /*! node-domexception. MIT License. Jimmy Wärting <https://jimmy.warting.se/opensource> */
 
-const DOMException = globalThis.DOMException ??= (() => {
+globalThis.DOMException ??= (() => {
   try { atob(0) } catch (err) { return err.constructor }
 })()
-
-export {
-  DOMException,
-  DOMException as default
-}
